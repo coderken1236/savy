d53aa18e8933c5d8c5f404add77bb1523e6a713b
coderken1236 <kenj092005@gmail.com>
2017-07-12 11:22:32 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -2,14 +2,31 @@
 # Date:
 
 
-""" 
-proj 03: Guessing Game
 
-Generate a random number between 1 and 9 (including 1 and 9). 
-Ask the user to guess the number, then tell them whether they guessed too low, too high, 
-or exactly right. Keep the game going until the user types exit.
-Keep track of how many guesses the user has taken, and when the game ends, print this out.
+# proj 03: Guessing Game
 
-"""
+# Generate a random number between 1 and 9 (including 1 and 9).
+# Ask the user to guess the number, then tell them whether they guessed too low, too high,
+# or exactly right. Keep the game going until the user types exit.
+# Keep track of how many guesses the user has taken, and when the game ends, print this out.
+import random
+loop_control = True
+random_number = random.randint(1,20)
+print "I am guessing a number between 1 and 20. Can you guess my number?"
+while loop_control == True:
+    guess = raw_input("Enter a number, or 'exit' to end the game: ")
+    if guess == 'exit':
+        loop_control = False
+    elif int(guess) > int(random_number):
+        print "Your number is too high"
+    elif int(guess) < int(random_number):
+        print "Your number is too low"
+    elif int(guess) == int(random_number):
+        print "Great! You guessed the number"
+        loop_control = False
+
+
+
+
 
 
